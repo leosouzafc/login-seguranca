0fe396541513094d3b3b40a22da201478dfe1c0e
Leonardo Souza <leonardo.souza@fcamara.com>
2025-04-17 17:28:39 -0300

    feat: add CORS middleware to allow cross-origin requests from frontend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI,HTTPException
 from backend.routers.auth import router as auth_router
 from backend.routers.users import router as user_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -8,11 +9,16 @@ def include_routers():
     app.include_router(auth_router, prefix="/auth", tags=["auth"])
     app.include_router(user_router, prefix="/users", tags=["users"])
 
-
-
 include_routers()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/ping")
 def health_check():
-    # raise HTTPException(status_code=500, detail="Erro forçado")
     return {"message":"pong"}
